2817664330716074cfa3214becba434e6207d7cd
unknown <cplascencia21@yahoo.com>
2018-06-06 16:59:55 -0700

    monster-fight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,32 @@ export class AppComponent {
   title = 'app';
 
   monsterName: string = 'kaiju';
+  monsterPower: number;
+
+  monster: any[] = [
+    {
+      monsterName: 'kaiju',
+      monsterPower: null
+    },
+    {
+      monsterName: 'smaug',
+      monsterPower: null
+    }
+  ];
+
+  monsterSelected;
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.pickMonster();
+  }
+
+  pickMonster() {
+    const index = Math.floor(Math.random () * this.monsterSelected.length);
+    this.monsterSelected = this.monsters[index];
+  }
+
+
+  generatePower() {
+    this.monsterSelected.monsterPower = Math.floor(Math.random() * 101);
+  }
 }
